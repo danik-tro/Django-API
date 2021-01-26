09d9ac78dcad7f2572797e8287398cc9219057ba
danik-tro <s.danik.tro.c@gmail.com>
2021-01-26 10:29:06 +0200

    Added new test for ESDB

--- api/elastic.py
+++ api/elastic.py
@@ -1,4 +1,5 @@
 from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search, Q
 import json
 
 
@@ -10,44 +11,72 @@ class ElasticSearchDB:
         }])
 
     def delete_item(self, index, doc_type, id_):
-        print(self.es.delete(index=index,
-                             doc_type=doc_type,
-                             id=id_))
+        return self.es.delete(index=index,
+                              doc_type=doc_type,
+                              id=id_)
 
     def delete_index(self, index):
-        self.es.indices.delete(index=index,
-                               ignore=[400, 404])
+        return self.es.indices.delete(index=index,
+                                      ignore=[400, 404])
 
     def create_index(self, index, doc_type, id_, body):
-        print(self.es.index(
+        return self.es.index(
             index=index,
             doc_type=doc_type,
             id=id_,
             body=body
-        ))
+        )
+
+    def get_mapping(self, index):
+        return self.es.indices.get_mapping(index=index)
 
     def get(self, index, doc_type, id_):
-        self.es.get(index=index,
-                    doc_type=doc_type,
-                    id=id_)
+        return self.es.get(index=index,
+                           doc_type=doc_type,
+                           id=id_)
 
 
-if __name__ == "__main__":
+def elastic_test_1():
     el_db = ElasticSearchDB()
 
-    el_db.create_index('test', 'test-doc', 1, {
+    print(el_db.create_index('test', 'test-doc', 1, {
         'test': 'test',
         'fieldsArray': [
             {'test_1': "test2"},
             {'test_2': "test3"},
             {'test_3': "test4"}
         ],
-    })
+    }))
 
-    el_db.get('test', 'test-doc', 1)
-
-    el_db.delete_item('test', 'test-doc', 1)
-
-    el_db.delete_index('test')
+    print(el_db.get('test', 'test-doc', 1))
+    print(el_db.delete_item('test', 'test-doc', 1))
+    print(el_db.delete_index('test'))
 
 
+def elastic_test_2():
+    import random
+
+    bodies = [{
+        "name": f"name_{i}",
+        "field": f'field_{i}',
+        "fields": [f'field_{random.randint(0, 100)}' for j in range(10)]
+    } for i in range(50)]
+
+    es = ElasticSearchDB()
+
+    for index, body in enumerate(bodies, 1):
+        es.create_index(index='test',
+                        doc_type='test-doc',
+                        id_=index,
+                        body=body)
+
+    print(es.get_mapping('test'))
+
+    for index in range(1, len(bodies)+1):
+        es.delete_item(index='test',
+                       doc_type='test-doc',
+                       id_=index)
+
+
+if __name__ == "__main__":
+    elastic_test_2()
